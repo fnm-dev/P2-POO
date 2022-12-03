454ac9e1b7b187df74147980e1a9f88f379a3a81
fnm-dev <f_nmachado@hotmail.com>
2022-12-03 11:59:17 -0300

    Interfaces das entidades do projeto

---
+++ src/model/dao/GasolineVehicleDao.java
@@ -0,0 +1,16 @@
+package model.dao;
+
+import model.entities.Category;
+import model.entities.Vehicle;
+
+import java.util.List;
+
+public interface GasolineVehicleDao {
+
+    void insert(Vehicle obj);
+    void update(Vehicle obj);
+    void deleteById(Integer id);
+    Vehicle findById(Integer id);
+    List<Vehicle> findAll();
+    List<Vehicle> findByCategory(Category category);
+}
